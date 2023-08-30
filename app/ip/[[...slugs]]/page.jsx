646bfbf53495ec372367621ac1875ca12d531a14
哲剑 <zhejian.wj@alipay.com>
2023-08-30 14:24:33 +0800

    fix

--- app/ip/[[...slugs]]/page.jsx
+++ app/ip/[[...slugs]]/page.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { headers } from 'next/headers';
 import Image from 'next/image'
+import InfisicalClient from 'infisical-node';
 
 
 const get_ipdata = async (ip) => {
-  const api_key = 'at_0iHJDJVsG3dqkwkwF2SeJQBAOnKMf';
-  const res = await fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${api_key}&ipAddress=${ip}`)
+  const client = new InfisicalClient({
+    token: process.env.KMS_TOKEN,
+    siteURL: 'https://kms.wangjun.dev',
+    cacheTTL: 3600
+  })
+  const IPIFY_API_KEY = await client.getSecret('IPIFY_API_KEY', {environment:'prod',path:'/',type:'shared'})
+
+  const res = await fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${IPIFY_API_KEY.secretValue}&ipAddress=${ip}`)
   return res.json();
 }
 
@@ -16,22 +23,23 @@ const IPPage = async ({ params }) => {
   const x_forward_for = headers().get("x-forwarded-for");
   const user_agent = headers().get('user-agent');
 
-  // 优先去参数ip，其次获取请求头中的ip
-  const ip = slugs 
-    ? slugs[0] : x_real_ip 
-    ? x_real_ip : x_forward_for 
-    ? x_forward_for : ' Not Found';
+  // 优先取参数ip，其次获取请求头中的ip
+  const ip = slugs ? slugs[0] : x_real_ip ? x_real_ip : x_forward_for ? x_forward_for : ' Not Found';
 
   const ip_data = await get_ipdata(ip);
   console.log('ip_data', ip_data);
 
   return (
-    <div class="p-3">
-    <Image src={`https://ipdata.co/flags/${ip_data.location.country.toLowerCase()}.png`} width={32} height={32}
-      alt="country" class="align-middle"/>
-      <span class="text-green-800 font-bold align-middle">
+    <div class="inline p-3">
+      <Image src={`https://ipdata.co/flags/${ip_data.location.country.toLowerCase()}.png`} width={32} height={32}
+      alt="country" class="align-middle inline"/>
+      <div class="text-green-800 font-bold align-middle pl-1 inline">
         {ip_data.ip}
-      </span>
+        <span class="inline text-gray-400 pl-1 text-xs">
+          {ip_data?.location.country},{ip_data?.location.region},{ip_data?.location.city}
+        </span>
+      </div>
+      <div class="text-xs text-gray-400 pl-3">{user_agent}</div>
     </div>    
   )
 }
