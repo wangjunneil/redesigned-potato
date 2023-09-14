1248abbdd33192f521d6217ccf8a081661f370ef
哲剑 <zhejian.wj@alipay.com>
2023-09-14 09:37:43 +0800

    fix

--- app/qiniu/route.js
+++ app/qiniu/route.js
@@ -1,21 +1,23 @@
-import qiniu from 'qiniu'
+import qiniu from "qiniu";
 
 export async function GET(request) {
-    var accessKey = 'aG8mlc9B4VMZe_9T282O7q3CbLCDdZ6YdohuDa8p';
-    var secretKey = '-7kLVkHoasqRjeR9zlT2e8KuRjJzKXEa-z2QtWsX';
-    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);    
+  var accessKey = "aG8mlc9B4VMZe_9T282O7q3CbLCDdZ6YdohuDa8p";
+  var secretKey = "-7kLVkHoasqRjeR9zlT2e8KuRjJzKXEa-z2QtWsX";
+  var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 
-    var options = {
-        scope: 'wjfilm',
-        returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}'
-    };
-    var putPolicy = new qiniu.rs.PutPolicy(options);
-    var uploadToken = putPolicy.uploadToken(mac);
+  var options = {
+    scope: "wjfilm",
+    expires: 86400,
+    returnBody:
+      '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}',
+  };
+  var putPolicy = new qiniu.rs.PutPolicy(options);
+  var uploadToken = putPolicy.uploadToken(mac);
 
-    return new Response(`{"status":"ok", "token": "${uploadToken}"}`, {
-        status: 200,
-        headers: {
-          'Content-type': 'application/json'
-        }
-    })
+  return new Response(`{"status":"ok", "token": "${uploadToken}"}`, {
+    status: 200,
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
 }
