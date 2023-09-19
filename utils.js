8cc76f30bc75185bb728ca93a5e342572a48409e
哲剑 <zhejian.wj@alipay.com>
2023-09-19 15:05:59 +0800

    fix

--- utils.js
+++ utils.js
@@ -1,14 +1,13 @@
 export const currentDate = () => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
 
-    return `${year}${month}${day}`;
-}
+  return `${year}${month}${day}`;
+};
 
 export const splitDate = () => {
-    const now = currentDate();
-    return now.match(/(\d{4})(\d{2})(\d{2})/).slice(1);
-}
-
+  const now = currentDate();
+  return now.match(/(\d{4})(\d{2})(\d{2})/).slice(1);
+};
