5a31d365b9f642dcf6ab575dc21b94b3fd772b47
哲剑 <zhejian.wj@alipay.com>
2023-10-12 13:35:59 +0800

    添加ip和json模块icon

--- app/json/page.jsx
+++ app/json/page.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "JSON Formatter",
+  description: "online json formatter",
+};
+
 const JsonViewPage = () => {
   return (
     <div style={{ width: "100%", height: "100vh", overflow: "auto" }}>
