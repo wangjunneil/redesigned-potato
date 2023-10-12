490e29e3f56acbffa38d33a5cc2bb7d515e68bf9
哲剑 <zhejian.wj@alipay.com>
2023-10-12 14:27:23 +0800

    pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   experimental: {
+//     serverActions: true,
+//     serverComponentsExternalPackages: ["mongoose"],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "ipdata.co",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "restapi.amap.com",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
@@ -16,6 +44,4 @@ const nextConfig = {
       },
     ],
   },
-};
-
-module.exports = nextConfig;
+});
