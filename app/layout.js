e9e1a08c2bd5242ba9dede4456c6abc06c8078d8
@zhejian123 <wangjunneil@gmail.com>
2023-10-23 01:26:58 -0500

    修改layout文件的metadata元素

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,21 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Life TimeLine",
-  description: "只是想做个记录",
-  viewport: "width=device-width, initial-scale=1.0",
-  manifest: "/manifest.json",
+  title: 'Life TimeLine',
+  description: '只是想做个记录',
+  viewport: 'width=device-width, initial-scale=1.0',
+  manifest: '/manifest.json',
+  themeColor: '#FFFFFF',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        style={{ background: "url(/bg.webp) repeat 5px 5px" }}
+        style={{ background: 'url(/bg.webp) repeat 5px 5px' }}
         className={inter.className}
       >
         {children}
