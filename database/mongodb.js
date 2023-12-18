773fe6c2b688c920dffcc8ef164f03d2932a5b69
哲剑 <zhejian.wj@alipay.com>
2023-12-18 15:32:10 +0800

    timelifes数据切换到mongodb cloud

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 import { getSecretValue } from "@/services";
 
 async function connectMongo() {
-  const MONGODB_URL = await getSecretValue("MONGODB_URL");
+  // 切换到 https://cloud.mongodb.com/
+  // const MONGODB_URL = await getSecretValue("MONGODB_URL");
+  const MONGODB_URL = process.env.MONGODB_URL;
 
   mongoose
     .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
